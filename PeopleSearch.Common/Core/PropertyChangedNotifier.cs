456eca28155112a31dcae63672a91d87045b3524
Daniel Comer <danielcomer@gmail.com>
2015-06-16 00:33:25 -0600

    Adding untracked files.

---
+++ PeopleSearch.Common/Core/PropertyChangedNotifier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace PeopleSearch.Common.Core
+{
+    public abstract class PropertyChangedNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void SetValue<T>(ref T value, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (value.Equals(newValue)) return;
+
+            value = newValue;
+            // ReSharper disable once ExplicitCallerInfoArgument
+            RaisePropertyChanged(propertyName);
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentNullException(nameof(propertyName));
+            }
+
+            VerifyProperty(propertyName);
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [Conditional("DEBUG")]
+        private void VerifyProperty(string propertyName)
+        {
+            var type = GetType();
+            var propInfo = type.GetRuntimeProperty(propertyName);
+
+            if (propInfo == null)
+            {
+                throw new ArgumentException("Invalid Property Name", nameof(propertyName));
+            }
+        }
+    }
+}
