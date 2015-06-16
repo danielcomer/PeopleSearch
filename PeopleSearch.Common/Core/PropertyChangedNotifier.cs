7091e0672e1fc035e9dc311b0a676963f4bf2839
Daniel Comer <danielcomer@gmail.com>
2015-06-16 06:12:54 -0600

    Fixed bug in SetValue<T>, added affectedProperties

--- PeopleSearch.Common/Core/PropertyChangedNotifier.cs
+++ PeopleSearch.Common/Core/PropertyChangedNotifier.cs
@@ -1,22 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+// ReSharper disable ExplicitCallerInfoArgument
 
 namespace PeopleSearch.Common.Core
 {
     public abstract class PropertyChangedNotifier : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged = delegate { }; 
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        
+        protected void SetValue<T>(ref T value, T newValue, IEnumerable<string> affectedProperties, [CallerMemberName] string propertyName = null)
+        {
+            if (value == null && newValue == null) return;
+            if (value != null && value.Equals(newValue)) return;
+
+            value = newValue;
+
+            RaisePropertyChanged(propertyName);
+
+            foreach (var property in affectedProperties)
+            {
+                RaisePropertyChanged(property);
+            }
+        }
 
         protected void SetValue<T>(ref T value, T newValue, [CallerMemberName] string propertyName = null)
         {
-            if (value.Equals(newValue)) return;
-
-            value = newValue;
-            // ReSharper disable once ExplicitCallerInfoArgument
-            RaisePropertyChanged(propertyName);
+            SetValue(ref value, newValue, Enumerable.Empty<string>(), propertyName);
         }
 
         protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
