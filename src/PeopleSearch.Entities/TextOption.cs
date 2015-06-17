f55cb6b2ded20924dd896d79237fe9a0116f5846
Daniel Comer <danielcomer@gmail.com>
2015-06-17 14:33:48 -0600

    removed unused file

--- src/PeopleSearch.Entities/TextOption.cs
+++ src/PeopleSearch.Entities/TextOption.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PeopleSearch.Entities
+﻿namespace PeopleSearch.Entities
 {
     public class TextOption
     {
