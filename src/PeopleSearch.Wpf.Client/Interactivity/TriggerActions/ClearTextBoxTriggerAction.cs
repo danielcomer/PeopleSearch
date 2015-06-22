ae8a8616be997b4ca8755f2b2f31b6eb2a48ce89
Daniel Comer <danielcomer@gmail.com>
2015-06-22 11:15:54 -0600

    For some reason this file was untracked.

---
+++ src/PeopleSearch.Wpf.Client/Interactivity/TriggerActions/ClearTextBoxTriggerAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace PeopleSearch.Wpf.Client.Interactivity.TriggerActions
+{
+    public class ClearTextBoxTriggerAction : TriggerAction<Button>
+    {
+        public static readonly DependencyProperty TargetProperty = DependencyProperty.Register("Target",
+            typeof (TextBox), typeof (ClearTextBoxTriggerAction), new UIPropertyMetadata(null));
+
+        public TextBox Target
+        {
+            get { return (TextBox)GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        protected override void Invoke(object parameter)
+        {
+            Target.Clear();
+            Target.Focus();
+        }
+    }
+}
